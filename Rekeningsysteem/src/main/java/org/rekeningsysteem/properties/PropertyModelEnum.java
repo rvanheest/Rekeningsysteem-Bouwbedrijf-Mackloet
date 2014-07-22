7be2eeab09257f98b07733f60658878e511b26fc
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-07-22 11:29:15 +0200

    Added project's resources for future use

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/properties/PropertyModelEnum.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/properties/PropertyModelEnum.java
@@ -14,20 +14,7 @@ public enum PropertyModelEnum implements PropertyKey {
 	UURLOON("uurloon"),
 
 	VALUTA("valuta"),
-	VALUTAISO4217("valutaiso4217"),
-
-	WONINGBOUW_NAAM("woningbouwnaam"),
-	WONINGBOUW_STRAAT("woningbouwstraat"),
-	WONINGBOUW_NUMMER("woningbouwnummer"),
-	WONINGBOUW_POSTCODE("woningbouwpostcode"),
-	WONINGBOUW_PLAATS("woningbouwplaats"),
-	WONINGBOUW_BTWNUMMER("woningbouwbtwnummer"),
-
-	PDF_AANGENOMEN_TEMPLATE("pdfaangenomentemplate"),
-	PDF_MUTATIES_TEMPLATE("pdfmutatiestemplate"),
-	PDF_OFFERTE_TEMPLATE("pdfoffertetemplate"),
-	PDF_PARTICULIER_TEMPLATE("pdfparticuliertemplate"),
-	PDF_REPARATIES_TEMPLATE("pdfreparatiestemplate");
+	VALUTAISO4217("valutaiso4217");
 
 	private String key;
 
