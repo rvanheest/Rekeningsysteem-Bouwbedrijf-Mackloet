e60756a80a35b9ab5719da266b5cd6c544c5823d
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-07-30 20:00:20 +0200

    Converter for special characters to LaTeX style

---
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/io/pdf/PdfConverter.java
@@ -0,0 +1,104 @@
+package org.rekeningsysteem.io.pdf;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import de.nixosoft.jlr.JLRConverter;
+
+public class PdfConverter extends JLRConverter {
+
+	private final Map<String, String> replaceMap = new HashMap<>();
+
+	public PdfConverter(File file) {
+		super(file);
+		this.fillMapWithSmallLetters();
+		this.fillMapWithCapitals();
+	}
+
+	private void fillMapWithSmallLetters() {
+		this.replaceMap.put("â", "\\^a");
+		this.replaceMap.put("ä", "\\\"a");
+		this.replaceMap.put("à", "\\`a");
+		this.replaceMap.put("á", "\\'a");
+		this.replaceMap.put("ê", "\\^e");
+		this.replaceMap.put("ë", "\\\"e");
+		this.replaceMap.put("è", "\\`e");
+		this.replaceMap.put("é", "\\'e");
+		this.replaceMap.put("î", "\\^i");
+		this.replaceMap.put("ï", "\\\"i");
+		this.replaceMap.put("ì", "\\`i");
+		this.replaceMap.put("í", "\\'i");
+		this.replaceMap.put("ô", "\\^o");
+		this.replaceMap.put("ö", "\\\"o");
+		this.replaceMap.put("ò", "\\`o");
+		this.replaceMap.put("ó", "\\'o");
+		this.replaceMap.put("û", "\\^u");
+		this.replaceMap.put("ü", "\\\"u");
+		this.replaceMap.put("ù", "\\`u");
+		this.replaceMap.put("ú", "\\'u");
+		this.replaceMap.put("ÿ", "\\\"y");
+		this.replaceMap.put("ý", "\\'y");
+	}
+
+	private void fillMapWithCapitals() {
+		this.replaceMap.put("Â", "\\^A");
+		this.replaceMap.put("Ä", "\\\"A");
+		this.replaceMap.put("À", "\\`A");
+		this.replaceMap.put("Á", "\\'A");
+		this.replaceMap.put("Ê", "\\^E");
+		this.replaceMap.put("Ë", "\\\"E");
+		this.replaceMap.put("È", "\\`E");
+		this.replaceMap.put("É", "\\'E");
+		this.replaceMap.put("Î", "\\^I");
+		this.replaceMap.put("Ï", "\\\"I");
+		this.replaceMap.put("Ì", "\\`I");
+		this.replaceMap.put("Í", "\\'I");
+		this.replaceMap.put("Ô", "\\^O");
+		this.replaceMap.put("Ö", "\\\"O");
+		this.replaceMap.put("Ò", "\\`O");
+		this.replaceMap.put("Ó", "\\'O");
+		this.replaceMap.put("Û", "\\^U");
+		this.replaceMap.put("Ü", "\\\"U");
+		this.replaceMap.put("Ù", "\\`U");
+		this.replaceMap.put("Ú", "\\'U");
+		this.replaceMap.put("Ý", "\\'Y");
+	}
+
+	@Override
+	public void replace(String key, Object value) {
+		super.replace(key, this.prepare(value));
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object prepare(Object value) {
+		if (value instanceof String) {
+			return this.prepare((String) value);
+		}
+		else if (value instanceof List) {
+			return this.prepare((List<Object>) value);
+		}
+		return value;
+	}
+
+	private String prepare(String value) {
+		String specials = "\\{}_^#&$%~";
+		return value.chars().parallel()
+				.mapToObj(c -> (char) c)
+				.map(c -> c.toString())
+				.map(s -> specials.contains(s) ? "\\" + s : s)
+				.map(s -> this.replaceMap.entrySet().stream()
+						.filter(entry -> s.equals(entry.getKey()))
+						.map(Entry::getValue)
+						.findFirst().orElse(s))
+				.map(s -> "\n".equals(s) ? "\\\\" : s)
+				.collect(Collectors.joining());
+	}
+
+	private List<Object> prepare(List<Object> list) {
+		return list.stream().map(this::prepare).collect(Collectors.toList());
+	}
+}
---
+++ Rekeningsysteem/src/test/java/org/rekeningsysteem/test/io/pdf/PdfConverterTest.java
@@ -0,0 +1,55 @@
+package org.rekeningsysteem.test.io.pdf;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.rekeningsysteem.io.pdf.PdfConverter;
+
+@RunWith(Parameterized.class)
+public class PdfConverterTest {
+
+	private final PdfConverter converter;
+	private final File file = new File("resources\\LaTeX\\Offerte.tex");
+	private final Object input;
+	private final Object expectedOutput;
+
+	public PdfConverterTest(Object input, Object expectedOutput) {
+		this.converter = new PdfConverter(this.file);
+		this.input = input;
+		this.expectedOutput = expectedOutput;
+	}
+
+	@Parameters
+	public static Collection<Object[]> data() {
+		Object[][] values = new Object[][] {
+				{ "test123", "test123" },
+				{ "test{1^2$3", "test\\{1\\^2\\$3" },
+				{ "testèn ís nïêt wÀt het lýkt",
+						"test\\`en \\'is n\\\"i\\^et w\\`At het l\\'ykt" },
+				{ "test\ntest123\n\ntest456", "test\\\\test123\\\\\\\\test456" },
+				{ Arrays.asList("123", "456", "{789}", "fôöbàr"),
+						Arrays.asList("123", "456", "\\{789\\}", "f\\^o\\\"ob\\`ar")
+				},
+				{ Arrays.asList(Arrays.asList("123", "456", "{789}", "fôöbàr"),
+						Arrays.asList("123", "456", "{789}", "fôöbàr"),
+						Arrays.asList("123", "456", "{789}", "fôöbàr")),
+						Arrays.asList(Arrays.asList("123", "456", "\\{789\\}", "f\\^o\\\"ob\\`ar"),
+								Arrays.asList("123", "456", "\\{789\\}", "f\\^o\\\"ob\\`ar"),
+								Arrays.asList("123", "456", "\\{789\\}", "f\\^o\\\"ob\\`ar"))
+				}
+		};
+		return Arrays.asList(values);
+	}
+
+	@Test
+	public void testPrepare() {
+		assertEquals(this.expectedOutput, this.converter.prepare(this.input));
+	}
+}
