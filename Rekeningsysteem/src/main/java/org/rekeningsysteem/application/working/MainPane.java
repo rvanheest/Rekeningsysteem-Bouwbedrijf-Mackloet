55b78bd1ffc27bd8af440110920b3715cb3ae67e
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-09-22 22:04:02 +0200

    Added the RekeningTabPane and some basic control for the buttons to open tabs

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/application/working/MainPane.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/application/working/MainPane.java
@@ -1,5 +1,8 @@
 package org.rekeningsysteem.application.working;
 
+import java.util.Collection;
+
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -7,29 +10,42 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import org.rekeningsysteem.application.Main;
+import org.rekeningsysteem.rxjavafx.Observables;
+
+import rx.Observable;
 
 import com.google.inject.Inject;
 
 public class MainPane extends BorderPane {
 
-	private RekeningToolbar toolbar;
+	private final RekeningToolbar toolbar;
+	private final RekeningTabPane tabpane;
 
-	private Button aangenomen;
-	private Button mutaties;
-	private Button reparaties;
-	private Button particulier;
-	private Button offerte;
-	private Button open;
-	private Button save;
-	private Button pdf;
-	private Button settings;
+	private final Button aangenomen = new Button();
+	private final Button mutaties = new Button();
+	private final Button reparaties = new Button();
+	private final Button particulier = new Button();
+	private final Button offerte = new Button();
+	private final Button open = new Button();
+	private final Button save = new Button();
+	private final Button pdf = new Button();
+	private final Button settings = new Button();
+	
+	private final Observable<ActionEvent> aangenomenEvents = Observables.fromNodeEvents(this.aangenomen, ActionEvent.ACTION);
+	private final Observable<ActionEvent> mutatiesEvents = Observables.fromNodeEvents(this.mutaties, ActionEvent.ACTION);
+	private final Observable<ActionEvent> reparatiesEvents = Observables.fromNodeEvents(this.reparaties, ActionEvent.ACTION);
+	private final Observable<ActionEvent> particulierEvents = Observables.fromNodeEvents(this.particulier, ActionEvent.ACTION);
+	private final Observable<ActionEvent> offerteEvents = Observables.fromNodeEvents(this.offerte, ActionEvent.ACTION);
+	private final Observable<ActionEvent> openEvents = Observables.fromNodeEvents(this.open, ActionEvent.ACTION);
+	private final Observable<ActionEvent> saveEvents = Observables.fromNodeEvents(this.save, ActionEvent.ACTION);
+	private final Observable<ActionEvent> pdfEvents = Observables.fromNodeEvents(this.pdf, ActionEvent.ACTION);
+	private final Observable<ActionEvent> settingsEvents = Observables.fromNodeEvents(this.settings, ActionEvent.ACTION);
 
 	@Inject
-	public MainPane(Stage stage) {
+	public MainPane(Stage stage, RekeningTabPane tabPane) {
 		this.setId("main-pane");
 		this.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
@@ -41,47 +57,84 @@ public class MainPane extends BorderPane {
 		this.toolbar = new RekeningToolbar(this.aangenomen, this.mutaties,
 				this.reparaties, this.particulier, this.offerte, this.open,
 				this.save, this.pdf, spacer, this.settings);
+		this.tabpane = tabPane;
 
 		this.setTop(this.toolbar);
-		this.setCenter(new StackPane());
+		this.setCenter(this.tabpane);
+		
+		Observable<Boolean> hasNoTabs = Observables.fromObservableList(this.tabpane.getTabs())
+				.map(Collection::isEmpty);
+		hasNoTabs.subscribe(this.save::setDisable);
+		hasNoTabs.subscribe(this.pdf::setDisable);
 	}
 
 	private void initButtons() {
-		this.aangenomen = new Button();
 		this.aangenomen.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/aangenomen.png"))));
 
-		this.mutaties = new Button();
 		this.mutaties.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/mutaties.png"))));
 
-		this.reparaties = new Button();
 		this.reparaties.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/reparaties.png"))));
 
-		this.particulier = new Button();
 		this.particulier.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/particulier.png"))));
 
-		this.offerte = new Button();
 		this.offerte.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/offerte.png"))));
 
-		this.open = new Button();
 		this.open.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/openen.png"))));
 
-		this.save = new Button();
 		this.save.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/opslaan.png"))));
 
-		this.pdf = new Button();
 		this.pdf.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/pdf.png"))));
 
-		this.settings = new Button();
 		this.settings.setId("settings-button");
 		this.settings.setGraphic(new ImageView(new Image(Main
 				.getResource("/images/settings.png"))));
+	}
+	
+	public RekeningTabPane getTabPane() {
+		return this.tabpane;
+	}
+
+	public Observable<ActionEvent> getAangenomenEvents() {
+		return this.aangenomenEvents;
+	}
+	
+	public Observable<ActionEvent> getMutatiesEvents() {
+		return this.mutatiesEvents;
+	}
+	
+	public Observable<ActionEvent> getReparatiesEvents() {
+		return this.reparatiesEvents;
+	}
+	
+	public Observable<ActionEvent> getParticulierEvents() {
+		return this.particulierEvents;
+	}
+	
+	public Observable<ActionEvent> getOfferteEvents() {
+		return this.offerteEvents;
+	}
+	
+	public Observable<ActionEvent> getOpenEvents() {
+		return this.openEvents;
+	}
+	
+	public Observable<ActionEvent> getSaveEvents() {
+		return this.saveEvents;
+	}
+	
+	public Observable<ActionEvent> getPdfEvents() {
+		return this.pdfEvents;
+	}
+	
+	public Observable<ActionEvent> getSettingsEvents() {
+		return this.settingsEvents;
 	}
 }
