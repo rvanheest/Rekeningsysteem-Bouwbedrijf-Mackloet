a35490ec830a01657053108fa8146851751cbd92
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-11-14 18:40:56 +0100

    integer and number formatted textfields

---
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/ui/textfields/IntegerField.java
@@ -0,0 +1,155 @@
+package org.rekeningsysteem.ui.textfields;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.IntegerPropertyBase;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ObjectPropertyBase;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.StringPropertyBase;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Control;
+
+public class IntegerField extends Control {
+
+	public static final int DEFAULT_PREF_COLUMN_COUNT = 12;
+
+	private ObjectProperty<Integer> value = new SimpleObjectProperty<Integer>(this, "aantal");
+
+	public final Integer getValue() {
+		return this.value.get();
+	}
+
+	public final void setValue(Integer value) {
+		this.value.set(value);
+	}
+
+	public final ObjectProperty<Integer> valueProperty() {
+		return this.value;
+	}
+
+	private BooleanProperty editable = new SimpleBooleanProperty(this, "editable", true);
+
+	public final boolean isEditable() {
+		return this.editable.getValue();
+	}
+
+	public final void setEditable(boolean value) {
+		this.editable.setValue(value);
+	}
+
+	public final BooleanProperty editableProperty() {
+		return this.editable;
+	}
+
+	private StringProperty promptText = new StringPropertyBase("") {
+
+		@Override
+		protected void invalidated() {
+			// Strip out newlines
+			String txt = get();
+			if (txt != null && txt.contains("\n")) {
+				txt = txt.replace("\n", "");
+				set(txt);
+			}
+		}
+
+		@Override
+		public Object getBean() {
+			return IntegerField.this;
+		}
+
+		@Override
+		public String getName() {
+			return "promptText";
+		}
+	};
+
+	public final StringProperty promptTextProperty() {
+		return this.promptText;
+	}
+
+	public final String getPromptText() {
+		return this.promptText.get();
+	}
+
+	public final void setPromptText(String value) {
+		this.promptText.set(value);
+	}
+
+	private IntegerProperty prefColumnCount = new IntegerPropertyBase(DEFAULT_PREF_COLUMN_COUNT) {
+
+		@Override
+		public void set(int value) {
+			if (value < 0) {
+				throw new IllegalArgumentException("value cannot be negative.");
+			}
+
+			super.set(value);
+		}
+
+		@Override
+		public Object getBean() {
+			return IntegerField.this;
+		}
+
+		@Override
+		public String getName() {
+			return "prefColumnCount";
+		}
+	};
+
+	public final IntegerProperty prefColumnCountProperty() {
+		return this.prefColumnCount;
+	}
+
+	public final int getPrefColumnCount() {
+		return this.prefColumnCount.getValue();
+	}
+
+	public final void setPrefColumnCount(int value) {
+		this.prefColumnCount.setValue(value);
+	}
+
+	private ObjectProperty<EventHandler<ActionEvent>> onAction = new ObjectPropertyBase<EventHandler<ActionEvent>>() {
+
+		@Override
+		protected void invalidated() {
+			IntegerField.this.setEventHandler(ActionEvent.ACTION, get());
+		}
+
+		@Override
+		public Object getBean() {
+			return IntegerField.this;
+		}
+
+		@Override
+		public String getName() {
+			return "onAction";
+		}
+	};
+
+	public final ObjectProperty<EventHandler<ActionEvent>> onActionProperty() {
+		return this.onAction;
+	}
+
+	public final EventHandler<ActionEvent> getOnAction() {
+		return this.onActionProperty().get();
+	}
+
+	public final void setOnAction(EventHandler<ActionEvent> value) {
+		this.onActionProperty().set(value);
+	}
+
+	public IntegerField() {
+		this.getStyleClass().setAll("integer-field");
+	}
+
+	@Override
+	protected String getUserAgentStylesheet() {
+		return this.getClass().getResource("/integerfield.css").toExternalForm();
+	}
+}
---
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/ui/textfields/NumberFieldSkin.java
@@ -0,0 +1,186 @@
+package org.rekeningsysteem.ui.textfields;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.Optional;
+
+import javafx.beans.InvalidationListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Skin;
+import javafx.scene.control.TextField;
+
+public class NumberFieldSkin implements Skin<NumberField> {
+
+	private NumberField control;
+	private TextField textField;
+	private DecimalFormat formatter;
+	private boolean ignoreValueUpdate = false;
+
+	private final InvalidationListener numberFieldFocusListener = obs -> {
+		if (this.control.isFocused()) {
+			this.textField.requestFocus();
+		}
+	};
+	private final InvalidationListener numberFieldValueListener = obs -> {
+		if (!this.ignoreValueUpdate) {
+			this.updateText();
+		}
+	};
+	private final InvalidationListener numberFieldStyleClassListener = obs -> this.textField
+			.getStyleClass().setAll(this.control.getStyleClass());
+
+	public NumberFieldSkin(NumberField control) {
+		this.control = control;
+
+		this.textField = new TextField() {
+
+			@Override
+			public void replaceText(int start, int end, String text) {
+				String t = Optional.ofNullable(this.getText()).orElse("");
+				if (NumberFieldSkin.this.accept(t.substring(0, start) + text + t.substring(end))) {
+					super.replaceText(start, end, text.replace('.', ','));
+				}
+			}
+
+			@Override
+			public void replaceSelection(String text) {
+				String t = Optional.ofNullable(this.getText()).orElse("");
+				int start = Math.min(this.getAnchor(), this.getCaretPosition());
+				int end = Math.max(this.getAnchor(), this.getCaretPosition());
+				if (NumberFieldSkin.this.accept(t.substring(0, start) + text + t.substring(end))) {
+					super.replaceSelection(text.replace('.', ','));
+				}
+			}
+		};
+
+		this.textField.setId("text-field");
+		this.textField.getStyleClass().setAll(control.getStyleClass());
+		this.control.getStyleClass().addListener(this.numberFieldStyleClassListener);
+
+		// Align the text to the right
+		this.textField.setAlignment(Pos.BASELINE_RIGHT);
+		this.textField.promptTextProperty().bind(control.promptTextProperty());
+		this.textField.editableProperty().bind(control.editableProperty());
+		this.textField.prefColumnCountProperty().bind(control.prefColumnCountProperty());
+
+		// Whenever the value changes on the control, we need to update the text
+		// in the TextField. The only time this is not the case is when the update
+		// to the control happened as a result of an update in the text textField.
+		this.control.valueProperty().addListener(this.numberFieldValueListener);
+
+		// Whenever the text of the textField changes, we may need to
+		// update the value.
+		this.textField.textProperty().addListener(obs -> this.updateValue());
+
+		// Right now there is some funny business regarding focus in JavaFX. So
+		// we will just make sure the TextField gets focus whenever somebody tries
+		// to give it to the MoneyField. This isn't right, but we need to fix
+		// this in JavaFX, I don't think I can hack around it
+		this.textField.setFocusTraversable(false);
+		this.control.focusedProperty().addListener(this.numberFieldFocusListener);
+
+		this.textField.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent actionEvent) {
+				// Because TextFieldBehavior fires an action event on the parent of the TextField
+				// (maybe a misfeature?) I don't need to do this. But I think this is
+				// a bug, because having to add an empty event handler to get an
+				// event on the control is odd to say the least!
+				// control.fireEvent(new ActionEvent(textField, textField));
+			}
+		});
+
+		// Make sure the text is updated to the initial state of the PercentageField value
+		this.updateLocale();
+		this.updateText();
+	}
+
+	@Override
+	public NumberField getSkinnable() {
+		return this.control;
+	}
+
+	@Override
+	public Node getNode() {
+		return this.textField;
+	}
+
+	@Override
+	public void dispose() {
+		this.control.getStyleClass().removeListener(this.numberFieldStyleClassListener);
+		this.control.valueProperty().removeListener(this.numberFieldValueListener);
+		this.control.focusedProperty().removeListener(this.numberFieldFocusListener);
+		this.textField = null;
+	}
+
+	private boolean accept(String text) {
+		text = text.trim();
+		if (text.length() == 0) {
+			return true;
+		}
+		
+		String negativePrefix = "-";
+		if (text.startsWith(negativePrefix)) {
+			text = text.substring(negativePrefix.length()).trim();
+		}
+
+		int decimalSeparatorIndex = -1;
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+			if (decimalSeparatorIndex == -1 && (ch == ',' || ch == '.')) {
+				decimalSeparatorIndex = i;
+			}
+			else if (!Character.isDigit(ch)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	private void updateLocale() {
+		Locale locale = Locale.getDefault();
+		this.formatter = (DecimalFormat) NumberFormat.getNumberInstance(locale);
+		this.formatter.setParseBigDecimal(true);
+	}
+
+	private void updateText() {
+		Optional<BigDecimal> value = Optional.ofNullable(this.control.getValue());
+		this.textField.setText(value.map(this.formatter::format).orElse(""));
+	}
+
+	private void updateValue() {
+		try {
+			this.ignoreValueUpdate = true;
+			BigDecimal value = this.control.getValue();
+			BigDecimal newValue;
+			String text = this.textField.getText() == null ? "" : this.textField.getText().trim();
+			String negative = "-";
+			if (text.equals("") || text.equals(negative)) {
+				newValue = value == null ? null : BigDecimal.ZERO;
+			}
+			else {
+				Number n = this.formatter.parse(text);
+				newValue = n instanceof BigDecimal
+						? (BigDecimal) n
+								: new BigDecimal(n.doubleValue());
+			}
+			if (value != newValue && (value == null || newValue == null || !value.equals(newValue))) {
+				this.control.setValue(newValue);
+			}
+		}
+		catch (ParseException ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			this.ignoreValueUpdate = false;
+		}
+	}
+}
