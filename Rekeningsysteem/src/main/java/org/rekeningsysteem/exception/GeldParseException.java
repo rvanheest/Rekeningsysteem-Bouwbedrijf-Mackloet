f98c8916e96ec743088618881c41eaa384dc90df
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-05-04 15:32:12 +0200

    serialVersionUID for GeldParseException

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/exception/GeldParseException.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/exception/GeldParseException.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 
 public class GeldParseException extends ParseException {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 8879311665577070103L;
 
 	public GeldParseException(String err) {
 		super(err, -1);
