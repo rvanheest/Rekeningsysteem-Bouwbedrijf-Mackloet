7c68e0389fb83439179f141d96481c325d5f9271
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-05-30 14:26:16 +0200

    Abstracted the database interaction

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/logic/database/ArtikellijstDBInteraction.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/logic/database/ArtikellijstDBInteraction.java
@@ -1,40 +1,38 @@
 package org.rekeningsysteem.logic.database;
 
+import java.sql.ResultSet;
+
 import org.rekeningsysteem.data.particulier.EsselinkArtikel;
 import org.rekeningsysteem.data.util.Geld;
 import org.rekeningsysteem.io.database.Database;
+import org.rekeningsysteem.io.database.ExFunc1;
 import org.rekeningsysteem.io.database.QueryEnumeration;
 
 import rx.Observable;
 import rx.functions.Func1;
 
-public class ArtikellijstDBInteraction {
+public class ArtikellijstDBInteraction extends DBInteraction<EsselinkArtikel> {
 
 	private static final Func1<String, QueryEnumeration> artNrQuery = s -> () ->
 			"SELECT * FROM Artikellijst WHERE artikelnummer LIKE '" + s + "%';";
 	private static final Func1<String, QueryEnumeration> omschrQuery = s -> () ->
 			"SELECT * FROM Artikellijst WHERE omschrijving LIKE '%" + s + "%';";
 
-	private final Database database;
-
 	public ArtikellijstDBInteraction(Database database) {
-		this.database = database;
+		super(database);
 	}
 
 	public Observable<EsselinkArtikel> getWithArtikelnummer(String text) {
-		return this.getWith(text).call(artNrQuery);
+		return this.with(text).call(artNrQuery);
 	}
 	
 	public Observable<EsselinkArtikel> getWithOmschrijving(String text) {
-		return this.getWith(text).call(omschrQuery);
+		return this.with(text).call(omschrQuery);
 	}
 	
-	private Func1<Func1<String, QueryEnumeration>, Observable<EsselinkArtikel>> getWith(String text) {
-		return factory -> this.getFromDatabase(factory.call(text.replace("\'", "\'\'")));
-	}
-	
-	private Observable<EsselinkArtikel> getFromDatabase(QueryEnumeration query) {
-		return this.database.query(query, result -> {
+	@Override
+	ExFunc1<ResultSet, EsselinkArtikel> resultExtractor() {
+		return result -> {
 			String artNr = result.getString("artikelnummer");
 			String omschr = result.getString("omschrijving");
 			int prijsPer = result.getInt("prijsPer");
@@ -42,6 +40,6 @@ public class ArtikellijstDBInteraction {
 			Geld verkoopPrijs = new Geld(result.getDouble("verkoopprijs"));
 
 			return new EsselinkArtikel(artNr, omschr, prijsPer, eenheid, verkoopPrijs);
-		});
+		};
 	}
 }
---
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/logic/database/DBInteraction.java
@@ -0,0 +1,34 @@
+package org.rekeningsysteem.logic.database;
+
+import java.sql.ResultSet;
+
+import org.rekeningsysteem.io.database.Database;
+import org.rekeningsysteem.io.database.ExFunc1;
+import org.rekeningsysteem.io.database.QueryEnumeration;
+
+import rx.Observable;
+import rx.functions.Func0;
+import rx.functions.Func1;
+
+public abstract class DBInteraction<T> {
+	
+	private final Database database;
+	
+	DBInteraction(Database database) {
+		this.database = database;
+	}
+
+	Func1<Func0<QueryEnumeration>, Observable<T>> with() {
+		return factory -> this.getFromDatabase(factory.call());
+	}
+	
+	Func1<Func1<String, QueryEnumeration>, Observable<T>> with(String text) {
+		return factory -> this.getFromDatabase(factory.call(text.replace("\'", "\'\'")));
+	}
+	
+	Observable<T> getFromDatabase(QueryEnumeration query) {
+		return this.database.query(query, this.resultExtractor());
+	};
+	
+	abstract ExFunc1<ResultSet, T> resultExtractor();
+}
