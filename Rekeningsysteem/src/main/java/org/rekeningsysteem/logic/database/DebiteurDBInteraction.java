498160b7475a1aabfa17f7bde38ded7df1ecfa3d
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-05-30 17:16:31 +0200

    Add debiteur feature to DebiteurDBInteraction

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/logic/database/DebiteurDBInteraction.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/logic/database/DebiteurDBInteraction.java
@@ -10,6 +10,7 @@ import org.rekeningsysteem.io.database.QueryEnumeration;
 import rx.Observable;
 import rx.functions.Func0;
 import rx.functions.Func1;
+import rx.functions.Func2;
 
 public class DebiteurDBInteraction extends DBInteraction<Debiteur> {
 
@@ -17,15 +18,38 @@ public class DebiteurDBInteraction extends DBInteraction<Debiteur> {
 			"SELECT * FROM Debiteur NATURAL LEFT JOIN BTWDebiteur WHERE naam LIKE '%" + s + "%';";
 	private static final Func0<QueryEnumeration> allQuery = () -> () ->
 			"SELECT * FROM Debiteur NATURAL LEFT JOIN BTWDebiteur;";
-	
+	private static final Func1<Debiteur, String> idQuery = deb -> "SELECT Debiteur.debiteurID "
+			+ "FROM Debiteur WHERE naam = '" + deb.getNaam() + "' AND straat = '" + deb.getStraat()
+			+ "' AND nummer = '" + deb.getNummer() + "' AND postcode = '" + deb.getPostcode()
+			+ "' AND plaats = '" + deb.getPlaats() + "'";
+
+	private static final Func1<Debiteur, QueryEnumeration> debInsertFunc = deb -> () ->
+			"INSERT INTO Debiteur (naam, straat, nummer, postcode, plaats) VALUES ('"
+					+ deb.getNaam() + "', '" + deb.getStraat() + "', '"
+					+ deb.getNummer() + "', '" + deb.getPostcode() + "', '"
+					+ deb.getPlaats() + "');";
+	private static final Func2<Debiteur, String, QueryEnumeration> btwInsert = (deb, num) -> () ->
+			"INSERT INTO BTWDebiteur (debiteurID, btwNummer) VALUES (("
+					+ idQuery.call(deb) + "), '" + num + "');";
+
 	public DebiteurDBInteraction(Database database) {
 		super(database);
+	}
+
+	public void addDebiteur(Debiteur debiteur) {
+		Func0<QueryEnumeration> debInsert = () -> debInsertFunc.call(debiteur);
+		QueryEnumeration insert = debiteur.getBtwNummer()
+				.map(num -> btwInsert.call(debiteur, num))
+				.map(debInsert.call()::append)
+				.orElseGet(() -> debInsert.call());
+
+		this.update(insert);
 	}
 
 	public Observable<Debiteur> getAll() {
 		return this.with().call(allQuery);
 	}
-	
+
 	public Observable<Debiteur> getWithNaam(String text) {
 		return this.with(text).call(naamQuery);
 	}
@@ -40,7 +64,7 @@ public class DebiteurDBInteraction extends DBInteraction<Debiteur> {
 			String postcode = result.getString("postcode");
 			String plaats = result.getString("plaats");
 			String btwNummer = result.getString("btwNummer");
-			
+
 			return new Debiteur(id, naam, straat, nummer, postcode, plaats, btwNummer);
 		};
 	}
