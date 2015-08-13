19e1f5a5fe4e7ffb7d9dda39ad3d5f7eb2434d0c
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-08-13 16:27:33 +0200

    Moved TotalenTest to other location

--- Rekeningsysteem/src/test/java/org/rekeningsysteem/test/data/util/TotalenTest.java
+++ Rekeningsysteem/src/test/java/org/rekeningsysteem/test/data/util/TotalenTest.java
@@ -1,4 +1,4 @@
-package org.rekeningsysteem.test.logic.bedragmanager;
+package org.rekeningsysteem.test.data.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
