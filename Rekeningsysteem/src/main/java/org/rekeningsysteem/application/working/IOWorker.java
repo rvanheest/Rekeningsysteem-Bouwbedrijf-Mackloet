3c1e3bb0a4c27dc149f4116664c190d342b9c7de
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-08-14 13:09:44 +0200

    Second old xml loader implemented in the IOWorker

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/application/working/IOWorker.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/application/working/IOWorker.java
@@ -10,6 +10,7 @@ import org.rekeningsysteem.io.pdf.PdfExporter;
 import org.rekeningsysteem.io.xml.XmlReader1;
 import org.rekeningsysteem.io.xml.XmlMaker;
 import org.rekeningsysteem.io.xml.XmlReader;
+import org.rekeningsysteem.io.xml.XmlReader2;
 
 import rx.Observable;
 
@@ -18,21 +19,24 @@ public class IOWorker {
 	private final FactuurSaver saver;
 	private final FactuurExporter exporter;
 	private final FactuurLoader loader;
-	private final FactuurLoader oldLoader;
+	private final FactuurLoader oldLoader1;
+	private final FactuurLoader oldLoader2;
 
 	public IOWorker() {
-		this(new XmlMaker(), new PdfExporter(), new XmlReader(), new XmlReader1());
+		this(new XmlMaker(), new PdfExporter(), new XmlReader(), new XmlReader1(), new XmlReader2());
 	}
 
 	public IOWorker(FactuurSaver saver, FactuurExporter exporter, FactuurLoader loader,
-			FactuurLoader oldLoader) {
+			FactuurLoader oldLoader1, FactuurLoader oldLoader2) {
 		this.saver = saver;
 		this.exporter = exporter;
 		this.loader = loader;
-		this.oldLoader = oldLoader;
+		this.oldLoader1 = oldLoader1;
+		this.oldLoader2 = oldLoader2;
 	}
 
 	public void save(AbstractRekening rekening, File file) {
+		System.out.println("save " + rekening + " in " + file);
 		this.saver.save(rekening, file);
 	}
 
@@ -41,6 +45,8 @@ public class IOWorker {
 	}
 
 	public Observable<? extends AbstractRekening> load(File file) {
-		return this.loader.load(file).onErrorResumeNext(t -> this.oldLoader.load(file));
+		return this.loader.load(file)
+				.onErrorResumeNext(t -> this.oldLoader2.load(file))
+				.onErrorResumeNext(t -> this.oldLoader1.load(file));
 	}
 }
