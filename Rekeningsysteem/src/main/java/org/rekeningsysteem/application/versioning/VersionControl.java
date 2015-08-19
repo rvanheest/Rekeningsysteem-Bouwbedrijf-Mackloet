cc06a3d860112467803cc4b04f0146d0727f36d0
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-08-19 10:11:35 +0200

    New version number (v0.4b-alpha)

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/application/versioning/VersionControl.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/application/versioning/VersionControl.java
@@ -5,12 +5,16 @@ import org.rekeningsysteem.io.database.QueryEnumeration;
 
 import rx.Observable;
 import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 public class VersionControl {
 
 	private static final Func1<String, QueryEnumeration> insertVersion = version -> () ->
 			"INSERT INTO Metadata VALUES ('" + version + "');";
+	private static final Func2<String, String, QueryEnumeration> updateVersion =
+			(oldV, newV) -> () -> "UPDATE Metadata SET version = '" + newV
+					+ "' WHERE version = '" + oldV + "';";
 
 	private final Database database;
 
@@ -30,7 +34,8 @@ public class VersionControl {
 		}
 		return this.getTableCount().map(count -> count == 0
 				? "" // Database is newly created
-				: "v0.3-alpha"); // version v0.3-alpha or less applies, which doesn't have a version table
+				: "v0.3-alpha"); // version v0.3-alpha or less applies, which doesn't have a version
+								 // table
 	}
 
 	private Observable<Integer> updateDatabase(String dbVersion) {
@@ -44,8 +49,14 @@ public class VersionControl {
 			return this.initDatabase(mavenVersion);
 		}
 		else if ("v0.3-alpha".equals(dbVersion)) {
-			QueryEnumeration query = getV04Queries()
+			// Database is in version v0.3-alpha
+			QueryEnumeration query = getV04AlphaQueries()
 					.append(insertVersion.call(mavenVersion));
+			return this.database.update(query).subscribeOn(Schedulers.io());
+		}
+		else if ("v0.4".equals(dbVersion) || "v0.4-alpha".equals(dbVersion)) {
+			// Database is in version v0.4-alpha
+			QueryEnumeration query = updateVersion.call(dbVersion, mavenVersion);
 			return this.database.update(query).subscribeOn(Schedulers.io());
 		}
 		return Observable.error(new IllegalArgumentException("the version of the database "
@@ -53,7 +64,7 @@ public class VersionControl {
 	}
 
 	public static String getMavenVersion() {
-		return "v0.4-alpha";
+		return "v0.4b-alpha";
 		// TODO switch lines
 //		return VersionControl.class.getPackage().getImplementationVersion();
 	}
@@ -80,20 +91,20 @@ public class VersionControl {
 
 	public Observable<Integer> initDatabase(String version) {
 		QueryEnumeration query = V03AlphaQueries.CREATE_ARTIKELLIJST
-				.append(getV04Queries())
+				.append(getV04AlphaQueries())
 				.append(insertVersion.call(version));
 
 		return this.database.update(query);
 	}
 
-	public static QueryEnumeration getV03Queries() {
+	public static QueryEnumeration getV03AlphaQueries() {
 		return V03AlphaQueries.CREATE_ARTIKELLIJST
 				.append(V03AlphaQueries.CREATE_DEBITEUR)
 				.append(V03AlphaQueries.CREATE_ART_NR_INDEX)
 				.append(V03AlphaQueries.CREATE_NAME_SEARCH);
 	}
 
-	public static QueryEnumeration getV04Queries() {
+	public static QueryEnumeration getV04AlphaQueries() {
 		return V04AlphaQueries.DROP_ART_NR_INDEX
 				.append(V04AlphaQueries.DROP_NAME_SEARCH_INDEX)
 				.append(V04AlphaQueries.DROP_DEBITEUR_TABLE)
