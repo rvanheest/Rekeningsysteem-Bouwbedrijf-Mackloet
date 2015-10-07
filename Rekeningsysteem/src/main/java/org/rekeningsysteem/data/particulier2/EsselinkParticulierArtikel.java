ac5e432f672da25c0cbff58d7c0059072de6d547
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2015-10-07 23:21:28 +0200

    New implementation for gebruikt Esselink artikel

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/data/particulier2/EsselinkParticulierArtikel.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/data/particulier2/EsselinkParticulierArtikel.java
@@ -3,12 +3,17 @@ package org.rekeningsysteem.data.particulier2;
 import java.util.Objects;
 
 import org.rekeningsysteem.data.particulier.EsselinkArtikel;
+import org.rekeningsysteem.data.util.Geld;
 import org.rekeningsysteem.data.util.visitor.ListItemVisitor;
 
 // TODO GebruiktEsselinkArtikel
-public class EsselinkParticulierArtikel extends ParticulierArtikel2Impl {
+public class EsselinkParticulierArtikel implements ParticulierArtikel2 {
 
+	private final String omschrijving;
+	private final Geld prijs;
+	private final double btwPercentage;
 	private final EsselinkArtikel artikel;
+	private final double aantal;
 
 	public EsselinkParticulierArtikel(EsselinkArtikel artikel, double aantal, double btwPercentage) {
 		this(artikel.getOmschrijving(), artikel, aantal, btwPercentage);
@@ -16,15 +21,44 @@ public class EsselinkParticulierArtikel extends ParticulierArtikel2Impl {
 
 	public EsselinkParticulierArtikel(String omschrijving, EsselinkArtikel artikel,
 			double aantal, double btwPercentage) {
-		super(omschrijving,
-				artikel.getVerkoopPrijs().multiply(aantal).divide(artikel.getPrijsPer()),
-				btwPercentage);
+		this.omschrijving = omschrijving;
+		this.prijs = artikel.getVerkoopPrijs().multiply(aantal).divide(artikel.getPrijsPer());
+		this.btwPercentage = btwPercentage;
+		this.aantal = aantal;
 		this.artikel = artikel;
+	}
+	
+	public String getOmschrijving() {
+		return this.omschrijving;
+	}
+
+	@Override
+	public Geld getMateriaal() {
+		return this.prijs;
+	}
+
+	@Override
+	public double getMateriaalBtwPercentage() {
+		return this.btwPercentage;
+	}
+
+	@Override
+	public final Geld getLoon() {
+		return new Geld(0);
+	}
+
+	@Override
+	public final double getLoonBtwPercentage() {
+		return 0;
 	}
 
 	public EsselinkArtikel getArtikel() {
 		return this.artikel;
 	}
+
+	public double getAantal() {
+    	return this.aantal;
+    }
 
 	@Override
 	public <T> T accept(ListItemVisitor<T> visitor) {
@@ -35,7 +69,10 @@ public class EsselinkParticulierArtikel extends ParticulierArtikel2Impl {
 	public boolean equals(Object other) {
 		if (other instanceof EsselinkParticulierArtikel) {
 			EsselinkParticulierArtikel that = (EsselinkParticulierArtikel) other;
-			return super.equals(that)
+			return Objects.equals(this.omschrijving, that.omschrijving)
+					&& Objects.equals(this.prijs, that.prijs)
+					&& Objects.equals(this.btwPercentage, that.btwPercentage)
+					&& Objects.equals(this.aantal, that.aantal)
 					&& Objects.equals(this.artikel, that.artikel);
 		}
 		return false;
@@ -43,14 +80,16 @@ public class EsselinkParticulierArtikel extends ParticulierArtikel2Impl {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(super.hashCode(), this.artikel);
+		return Objects.hash(this.omschrijving, this.prijs, this.btwPercentage,
+				this.aantal, this.artikel);
 	}
 
 	@Override
 	public String toString() {
-		return "<EsselinkParticulierArtikel[" + String.valueOf(this.getOmschrijving()) + ", "
-				+ String.valueOf(this.getMateriaal()) + ", "
-				+ String.valueOf(this.getMateriaalBtwPercentage()) + ", "
+		return "<EsselinkParticulierArtikel[" + String.valueOf(this.omschrijving) + ", "
+				+ String.valueOf(this.prijs) + ", "
+				+ String.valueOf(this.btwPercentage) + ", "
+				+ String.valueOf(this.aantal) + ", "
 				+ String.valueOf(this.artikel) + "]>";
 	}
 }
