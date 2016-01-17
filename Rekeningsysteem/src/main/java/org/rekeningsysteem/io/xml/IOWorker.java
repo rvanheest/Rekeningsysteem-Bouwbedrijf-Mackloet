0b26a9e8657908496ff5698716ad433e893334bd
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2016-01-17 22:45:19 +0100

    New IOWorker

---
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/io/xml/IOWorker.java
@@ -0,0 +1,56 @@
+package org.rekeningsysteem.io.xml;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.rekeningsysteem.data.util.AbstractRekening;
+import org.rekeningsysteem.io.FactuurExporter;
+import org.rekeningsysteem.io.FactuurLoader;
+import org.rekeningsysteem.io.FactuurSaver;
+import org.rekeningsysteem.io.pdf.PdfExporter;
+
+import rx.Observable;
+
+public class IOWorker implements FactuurSaver, FactuurExporter, FactuurLoader {
+
+	private final FactuurSaver saver;
+	private final FactuurExporter exporter;
+	private final FactuurLoader loader;
+	private final FactuurLoader oldLoader1;
+	private final FactuurLoader oldLoader2;
+	private final FactuurLoader oldLoader3;
+
+	public IOWorker(Logger logger) {
+		this(new XmlMaker(logger), new PdfExporter(logger), new XmlReader(logger),
+				new XmlReader1(logger), new XmlReader2(logger), new XmlReader3(logger), logger);
+	}
+
+	public IOWorker(FactuurSaver saver, FactuurExporter exporter, FactuurLoader loader,
+			FactuurLoader oldLoader1, FactuurLoader oldLoader2, FactuurLoader oldLoader3,
+			Logger logger) {
+		this.saver = saver;
+		this.exporter = exporter;
+		this.loader = loader;
+		this.oldLoader1 = oldLoader1;
+		this.oldLoader2 = oldLoader2;
+		this.oldLoader3 = oldLoader3;
+	}
+
+	@Override
+	public Observable<AbstractRekening> load(File file) {
+		return this.loader.load(file)
+				.onErrorResumeNext(t -> this.oldLoader3.load(file))
+				.onErrorResumeNext(t -> this.oldLoader2.load(file))
+				.onErrorResumeNext(t -> this.oldLoader1.load(file));
+	}
+
+	@Override
+	public void export(AbstractRekening rekening, File saveLocation) {
+		this.exporter.export(rekening, saveLocation);
+	}
+
+	@Override
+	public void save(AbstractRekening rekening, File saveLocation) {
+		this.saver.save(rekening, saveLocation);
+	}
+}
