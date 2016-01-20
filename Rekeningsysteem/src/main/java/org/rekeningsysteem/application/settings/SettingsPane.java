3a7cfc43f587ab43a96af4ad1fb070069c0c77c8
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2016-01-20 16:42:49 +0100

    Forgotten DebiteurTablePane in SettingsPane refactoring

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/application/settings/SettingsPane.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/application/settings/SettingsPane.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import org.apache.log4j.Logger;
 import org.rekeningsysteem.application.settings.debiteur.DebiteurTableController;
+import org.rekeningsysteem.application.settings.debiteur.DebiteurTablePane;
 import org.rekeningsysteem.application.settings.offerte.DefaultOfferteTextPaneController;
 import org.rekeningsysteem.io.database.Database;
 import org.rekeningsysteem.logic.database.ArtikellijstDBInteraction;
@@ -20,7 +21,7 @@ public class SettingsPane extends TabPane {
 
 		DebiteurDBInteraction dbInteraction = new DebiteurDBInteraction(database);
 		DebiteurTableController debC = new DebiteurTableController(dbInteraction);
-		Tab debiteurTab = new Tab("Debiteur beheer", debC.getUI());
+		Tab debiteurTab = new Tab("Debiteur beheer", new DebiteurTablePane(debC.getUI()));
 
 		PrijslijstIO prijslijstTab = new PrijslijstIO(stage,
 				new ArtikellijstDBInteraction(database), closeButton, logger);
