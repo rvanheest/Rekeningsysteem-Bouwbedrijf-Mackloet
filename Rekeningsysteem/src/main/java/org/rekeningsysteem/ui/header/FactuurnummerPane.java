49ef594a94dda9477c7460d16bc8ab3d106976ad
Richard van Heest <richard.v.heest@gmail.com>
2017-10-20 20:12:27 +0200

    correct naming for offertenummer in UI

--- Rekeningsysteem/src/main/java/org/rekeningsysteem/ui/header/FactuurnummerPane.java
+++ Rekeningsysteem/src/main/java/org/rekeningsysteem/ui/header/FactuurnummerPane.java
@@ -13,13 +13,25 @@ import rx.Observable;
 public class FactuurnummerPane extends Page {
 
 	public enum FactuurnummerType {
-		FACTUUR("Factuurnummer"),
-		OFFERTE("Offertenummer");
+		FACTUUR("Factuurnummer", "factuurnummer", "factuur"),
+		OFFERTE("Offertenummer", "offertenummer", "offerte");
 
+		private final String name;
+		private final String name_lowercase;
 		private final String type;
 
-		private FactuurnummerType(String type) {
+		FactuurnummerType(String name, String name_lowercase, String type) {
+			this.name = name;
+			this.name_lowercase = name_lowercase;
 			this.type = type;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public String getNameLowercase() {
+			return this.name_lowercase;
 		}
 
 		public String getType() {
@@ -27,18 +39,21 @@ public class FactuurnummerPane extends Page {
 		}
 	}
 
-	private static final String emptyText = "Er is nog geen factuurnummer toegekend aan deze factuur";
+	private final String emptyText;
 
-	private final Label factNrL = new Label(emptyText);
+	private final Label factNrL;
 	private final Observable<Optional<String>> factuurnummer;
 
 	public FactuurnummerPane(FactuurnummerType type) {
-		super(type.getType());
+		super(type.getName());
+
+		this.emptyText = String.format("Er is nog geen %s toegekend aan deze %s", type.getNameLowercase(), type.getType());
+		this.factNrL = new Label(this.emptyText);
 
 		this.getChildren().add(this.factNrL);
 
 		this.factuurnummer = Observables.fromProperty(this.factNrL.textProperty())
-				.map(s -> Objects.isNull(s) || s.isEmpty() || emptyText.equals(s)
+				.map(s -> Objects.isNull(s) || s.isEmpty() || this.emptyText.equals(s)
 						? Optional.empty()
 						: Optional.of(s));
 	}
@@ -48,6 +63,6 @@ public class FactuurnummerPane extends Page {
 	}
 
 	public void setFactuurnummer(Optional<String> factuurnummer) {
-		this.factNrL.setText(factuurnummer.orElse(emptyText));
+		this.factNrL.setText(factuurnummer.orElse(this.emptyText));
 	}
 }
